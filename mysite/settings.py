7da00aff8313913091c33a0fe8bfff0d9eedf535
Mukesh Kumar Ray <mukeshray995531@gmail.com>
2026-01-30 21:05:19 +0530

    Fix static CSS path

--- mysite/settings.py
+++ mysite/settings.py
@@ -121,4 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
